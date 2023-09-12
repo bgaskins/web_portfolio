bae396878516b9c945986517b26a0f5a7b2ca328
Bryan Gaskins <bryandgaskins@gmail.com>
2023-09-12 15:16:56 -0400

    Added download button and * icon

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faAsterisk } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './Navbar.css';
 
@@ -13,10 +14,14 @@ class Navbar extends Component {
             <h2>Bryan Gaskins</h2>
           </div>
               <ul className="nav-list">
-                  <a className="nav-link active" href="https://www.linkedin.com/in/bryangaskins">
-                    <FontAwesomeIcon icon={faLinkedin} size = 'lg' id="linkedin" /></a>
-                  <a className="nav-link active" href="https://github.com/bgaskins">
-                    <FontAwesomeIcon icon={faGithub} size = 'lg' id="github" /></a>
+              <FontAwesomeIcon id="asterisk" icon={faAsterisk} size = '2x' className="me-4 mt-2"/>
+                <button>
+                  <a id="resume" href="./assets/BryanGaskins-Resume.pdf" download="BryanGaskins-Resume.pdf">Resume</a>
+                </button>
+                <a className="nav-link active" href="https://www.linkedin.com/in/bryangaskins">
+                  <FontAwesomeIcon id="linkedin" icon={faLinkedin} size = 'lg' /></a>
+                <a className="nav-link active" href="https://github.com/bgaskins">
+                  <FontAwesomeIcon id="github" icon={faGithub} size = 'lg' /></a>
               </ul>
           </div>
       </nav>
