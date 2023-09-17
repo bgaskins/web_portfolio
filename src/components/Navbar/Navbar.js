422fd25228da31e0656d55b5abe9c9b1cc4fb9fb
Bryan Gaskins <bryandgaskins@gmail.com>
2023-09-17 16:07:00 -0400

    Bug fix

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,7 +15,7 @@ class Navbar extends Component {
               <ul className="nav-list">
               <FontAwesomeIcon id="asterisk" icon={faAsterisk} size = '2x' className="me-4 mt-2"/>
                 <button>
-                  <a id="resume" href="https://bgwebportfolio.s3.amazonaws.com/BryanGaskins-Resume.pdf" download="BryanGaskins-Resume.pdf">Resume</a>
+                  <a id="resume" href="./assets/BryanGaskins-Resume.pdf" download="BryanGaskins-Resume.pdf">Resume</a>
                 </button>
                 <a className="nav-link active" href="https://www.linkedin.com/in/bryangaskins">
                   <FontAwesomeIcon id="linkedin" icon={faLinkedin} size = 'lg' /></a>
