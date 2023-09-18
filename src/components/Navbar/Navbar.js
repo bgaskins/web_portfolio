2e1e4530b5848d81d7c50802428ddc9a7dbcd301
Bryan Gaskins <bryandgaskins@gmail.com>
2023-09-18 12:25:17 -0400

    Removed import

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faAsterisk } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './Navbar.css';
 
